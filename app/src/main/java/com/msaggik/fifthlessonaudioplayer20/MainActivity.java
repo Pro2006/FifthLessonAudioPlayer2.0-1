a3ff5dcca604c69fb95a004bfe0b3ab2064d53ec
Pro2006 <niva11.2006.1@gmail.com>
2023-03-30 19:02:25 +0300

    Версия с работающим ползунком, то есть при нажатии паузы не сбрасывается прогресс

--- app/src/main/java/com/msaggik/fifthlessonaudioplayer20/MainActivity.java
+++ app/src/main/java/com/msaggik/fifthlessonaudioplayer20/MainActivity.java
@@ -3,7 +3,9 @@ package com.msaggik.fifthlessonaudioplayer20;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
 import android.content.res.AssetFileDescriptor;
+import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
@@ -12,14 +14,17 @@ import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity  implements Runnable{
+public class MainActivity extends AppCompatActivity implements Runnable {
 
     // создание полей
     private MediaPlayer mediaPlayer = new MediaPlayer(); // создание поля медиа-плеера
     private SeekBar seekBar; // создание поля SeekBar
     private boolean wasPlaying = false; // поле проигрывания аудио-файла
-    private FloatingActionButton fabPlayPause; // поле кнопки проигрывания и постановки на паузу аудиофайла
-    private TextView seekBarHint; // поле информации у SeekBar
+    private FloatingActionButton fabPlayPause, fabNext, fabBack, fabLoop, fabForward; // поле кнопки проигрывания и постановки на паузу аудиофайла
+    private TextView seekBarHint, songInformation; // поле информации у SeekBar
+    private String metaDataAudio;
+    private boolean isRepeat = false;
+    int mediaPlayerStopProgress = 0;
 
 
     @Override
@@ -29,35 +34,34 @@ public class MainActivity extends AppCompatActivity  implements Runnable{
 
         // присваивание полям id ресурсов
         fabPlayPause = findViewById(R.id.fabPlayPause);
+        fabNext = findViewById(R.id.floatingActionNext);
+        fabForward = findViewById(R.id.floatingActionForward);
+        fabBack = findViewById(R.id.floatingButtonback);
+        fabLoop = findViewById(R.id.floatingActionLoop);
+        fabPlayPause = findViewById(R.id.fabPlayPause);
         seekBarHint = findViewById(R.id.seekBarHint);
         seekBar = findViewById(R.id.seekBar);
-
-        // создание слушателя нажатия кнопки fabPlayPause
-        fabPlayPause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                playSong(); // воспроизведение музыки
-            }
-        });
+        songInformation = findViewById(R.id.songInfomation);
 
         // создание слушателя изменения SeekBar
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             // метод при перетаскивании ползунка по шкале,
             // где progress позволяет получить нове значение ползунка (позже progress назрачается длина трека в миллисекундах)
+            @SuppressLint("SetTextI18n")
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
                 seekBarHint.setVisibility(View.VISIBLE); // установление видимости seekBarHint
                 //seekBarHint.setVisibility(View.INVISIBLE); // установление не видимости seekBarHint
 
                 // Math.ceil() - округление до целого в большую сторону
-                int timeTrack = (int) Math.ceil(progress/1000f); // перевод времени из миллисекунд в секунды
+                int timeTrack = (int) Math.ceil(progress / 1000f); // перевод времени из миллисекунд в секунды
 
                 // вывод на экран времени отсчёта трека
                 if (timeTrack < 10) {
                     seekBarHint.setText("00:0" + timeTrack);
-                } else if (timeTrack < 60){
+                } else if (timeTrack < 60) {
                     seekBarHint.setText("00:" + timeTrack);
-                } else if (timeTrack >= 60) {
+                } else {
                     seekBarHint.setText("01:" + (timeTrack - 60));
                 }
 
@@ -66,20 +70,22 @@ public class MainActivity extends AppCompatActivity  implements Runnable{
                 // seekBar.getX() - начало seekBar по оси Х
                 // seekBar.getWidth() - ширина контейнера seekBar
                 // 0.92 - поправочный коэффициент (так как seekBar занимает не всю ширину своего контейнера)
-                seekBarHint.setX(seekBar.getX() + Math.round(seekBar.getWidth()*percentTrack*0.92));
+                seekBarHint.setX(seekBar.getX() + Math.round(seekBar.getWidth() * percentTrack * 0.92));
 
                 if (progress > 0 && mediaPlayer != null && !mediaPlayer.isPlaying()) { // если mediaPlayer не пустой и mediaPlayer не воспроизводится
-                    clearMediaPlayer(); // остановка и очиска MediaPlayer
+                    mediaPlayer.pause(); // остановка и очиска MediaPlayer
                     // назначение кнопке картинки play
                     fabPlayPause.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, android.R.drawable.ic_media_play));
-                    MainActivity.this.seekBar.setProgress(0); // установление seekBar значения 0
+                    // установление seekBar значения 0
                 }
             }
+
             // метод при начале перетаскивания ползунка по шкале
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
                 seekBarHint.setVisibility(View.INVISIBLE); // установление видимости seekBarHint
             }
+
             // метод при завершении перетаскивания ползунка по шкале
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
@@ -88,14 +94,53 @@ public class MainActivity extends AppCompatActivity  implements Runnable{
                 }
             }
         });
+
+        fabPlayPause.setOnClickListener(listener);
+        fabBack.setOnClickListener(listener);
+        fabLoop.setOnClickListener(listener);
+        fabForward.setOnClickListener(listener);
     }
+
+    private final View.OnClickListener listener = new View.OnClickListener() {
+        @SuppressLint("NonConstantResourceId")
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.fabPlayPause:
+                    playSong();
+                    break;
+                case R.id.floatingButtonback:
+                    if (mediaPlayer != null) {
+                        mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() - 5000);
+                    }
+                    break;
+                case R.id.floatingActionForward:
+                    if (mediaPlayer != null) {
+                        mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 5000);
+                    }
+                    break;
+                case R.id.floatingActionLoop:
+                    if (mediaPlayer != null) {
+                        if (isRepeat) {
+                            isRepeat = false;
+                            mediaPlayer.setLooping(false);
+                        } else {
+                            isRepeat = true;
+                            mediaPlayer.setLooping(true);
+                        }
+                        break;
+                    }
+            }
+
+        }
+    };
 
     // метод запуска аудио-файла
     public void playSong() {
         try { // обработка исключения на случай отстутствия файла
             if (mediaPlayer != null && mediaPlayer.isPlaying()) { // если mediaPlayer не пустой и mediaPlayer воспроизводится
-                clearMediaPlayer(); // остановка и очиска MediaPlayer
-                seekBar.setProgress(0); // присваивание seekBar значения 0
+                mediaPlayerStopProgress = mediaPlayer.getCurrentPosition();
+                clearMediaPlayer();// остановка mediaPlayer
                 wasPlaying = true; // инициализация значения запуска аудио-файла
                 // назначение кнопке картинки play
                 fabPlayPause.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, android.R.drawable.ic_media_play));
@@ -105,13 +150,26 @@ public class MainActivity extends AppCompatActivity  implements Runnable{
                 if (mediaPlayer == null) { // если mediaPlayer пустой
                     mediaPlayer = new MediaPlayer(); // то выделяется для него память
                 }
-                // назначение кнопке картинки pause
-                fabPlayPause.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, android.R.drawable.ic_media_pause));
-
                 // альтернативный способ считывания файла с помощью файлового дескриптора
                 AssetFileDescriptor descriptor = getAssets().openFd("Н.А.Римский-Корсаков - Полёт шмеля.mp3");
                 // запись файла в mediaPlayer, задаются параметры (путь файла, смещение относительно начала файла, длина аудио в файле)
                 mediaPlayer.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
+
+                // получение мета-данных из аудио файла
+                MediaMetadataRetriever mediaMetadataRetriever = new MediaMetadataRetriever();
+                mediaMetadataRetriever.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
+                metaDataAudio = mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE) +
+                        " \n- ";
+                String author = mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+                if (author == null) {
+                    metaDataAudio += "Автор отсутствует";
+                } else {
+                    metaDataAudio += author;
+                }
+                mediaMetadataRetriever.release();
+                songInformation.setText(metaDataAudio);
+
+
                 descriptor.close(); // закрытие дескриптора
 
                 mediaPlayer.prepare(); // ассинхронная подготовка плейера к проигрыванию
@@ -119,8 +177,13 @@ public class MainActivity extends AppCompatActivity  implements Runnable{
                 mediaPlayer.setLooping(false); // назначение отстутствия повторов
                 seekBar.setMax(mediaPlayer.getDuration()); // ограниечение seekBar длинной трека
 
+
                 mediaPlayer.start(); // старт mediaPlayer
+                // назначение кнопке картинки pause
+                fabPlayPause.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, android.R.drawable.ic_media_pause));
                 new Thread(this).start(); // запуск дополнительного потока
+                mediaPlayer.seekTo(mediaPlayerStopProgress);
+                seekBar.setProgress(mediaPlayerStopProgress);
             }
 
             wasPlaying = false; // возврат отсутствия проигрывания mediaPlayer
